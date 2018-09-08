5818e689330594490c9e67eca391b6dbe823c858
MarycBrown02 <marycbrown01@sbcgloba.net>
2018-09-08 13:18:09 -0500

    added a folder and a file and changed html file

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+var win = 0;
+var loose =0;
+var gustLeft=0; 
+var gust=0;
+//pick random letter
+var myArray = letters[Math.floor(Math.random()*letters.length)];
+
+
+console.log(myArray)
+
+
+
+
+
+
+
+
+
+
